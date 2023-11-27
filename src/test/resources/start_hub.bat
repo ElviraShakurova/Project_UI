1e6091ef9adcb6d850247ae733638a9006ffc5b4
Elvira Shakurova <elenshakurova@gmail.com>
2023-11-27 23:48:25 +0300

    add change start_hub.bat and start_node.bat, remove hubConfig.json and nodeConfig.json

--- src/test/resources/start_hub.bat
+++ src/test/resources/start_hub.bat
@@ -1,3 +1,1 @@
-java -jar selenium-server-standalone-4.15.0.jar -role hub -hubConfig hub.json
-
 java -jar selenium-server-4.15.0.jar hub
--- src/test/resources/start_node.bat
+++ src/test/resources/start_node.bat
@@ -1,5 +1,1 @@
-java -jar selenium-server-standalone-4.15.0.jar -role node -nodeConfig node.json
-
-java -jar selenium-server-4.15.0.jar -Dwebdriver.chrome.driver=C:/WebDriver/bin/chromedriver.exe -Dwebdriver.gecko.driver=C:/WebDriver/bin/geckodriver-v0.33.0-win64/geckodriver.exe node --hub http://172.25.48.1:4444/grid/register
-
-java -jar selenium-server-4.15.0.jar node --hub http://172.25.48.1:4444/grid/register
+java -jar selenium-server-4.15.0.jar node
