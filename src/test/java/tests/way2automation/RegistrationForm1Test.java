df9c8e553b8674a27695f1f8dc9b664f61c27a28
Elvira Shakurova <elenshakurova@gmail.com>
2023-12-05 00:20:26 +0300

    add change EnvHelper, RegistrationForm1Test, .env_local.properties

--- src/test/java/tests/way2automation/RegistrationForm1Test.java
+++ src/test/java/tests/way2automation/RegistrationForm1Test.java
@@ -1,19 +1,23 @@
 package tests.way2automation;
 
 import io.qameta.allure.*;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import pages.JqueryPage;
 import pages.MainPage;
+import tests.base.BaseFactoryTest;
 import tests.base.GridBaseTest;
 
-public class RegistrationForm1Test extends GridBaseTest {
+import static java.sql.DriverManager.getDriver;
+
+public class RegistrationForm1Test extends BaseFactoryTest {
 
     @Severity(SeverityLevel.NORMAL)
     @Epic("Тесты формы регистрации")
     @Story("Проверка перехода с главной страницы по меню на страницу с формой регистрации 1")
     @Test
     public void testRegistrationForm1() {
-        MainPage mainPage = new MainPage(getDriver())
+        MainPage mainPage = new MainPage(driver)
                 .clickResourcesButton();
         JqueryPage jqueryPage = mainPage.clickPracticeSite1()
                 .assertRegistrationForm();
