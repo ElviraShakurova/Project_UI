8fa9b43a37f10e54ba2f08a15729a08ea6e6da64
Elvira Shakurova <elenshakurova@gmail.com>
2023-12-19 18:05:22 +0300

    add AuthenticationPage, BasicAuthBaseTest and BasicAuthTest

---
+++ src/test/java/pages/AuthenticationPage.java
@@ -0,0 +1,45 @@
+package pages;
+
+import helpers.EnvHelper;
+import io.qameta.allure.Step;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
+
+public class AuthenticationPage {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    @FindBy(id = "displayImage")
+    private WebElement displayImageButton;
+
+    @FindBy(id = "downloadImg")
+    private WebElement imageSuccessfullyAuthentication;
+
+    public AuthenticationPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        wait = new WebDriverWait(driver, EnvHelper.getExplicitWaitDuration());
+    }
+
+    @Step("Нажатие на кнопку Display Image")
+    public AuthenticationPage clickDisplayImageButton(){
+        displayImageButton.click();
+        return this;
+    }
+
+    @Step("Проверка отображения изображения об успешной авторизации")
+    public AuthenticationPage assertImageSuccessfullyBathAuthentication(){
+        wait.until(ExpectedConditions.visibilityOf(imageSuccessfullyAuthentication));
+        assertTrue(imageSuccessfullyAuthentication.isDisplayed());
+        return this;
+    }
+}
---
+++ src/test/java/tests/basicAuth/BasicAuthTest.java
@@ -0,0 +1,21 @@
+package tests.basicAuth;
+
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import org.testng.annotations.Test;
+import pages.AuthenticationPage;
+
+public class BasicAuthTest extends BasicAuthBaseTest {
+
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Базовая аутентификация")
+    @Story("Успешная аутентификация пользователя")
+    @Test
+    public void testBasicAuthentication() {
+        AuthenticationPage authenticationPage= new AuthenticationPage(driver)
+                .clickDisplayImageButton()
+                .assertImageSuccessfullyBathAuthentication();
+    }
+}
